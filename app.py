5a4df110fde76011e184ac88dbc635210ea6e199
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-11-17 20:29:43 +0200

    add functionality to send messages in conversations and to remove conversations (admin)

--- app.py
+++ app.py
@@ -13,7 +13,34 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    sql = "SELECT * FROM conversations"
+    result = db.session.execute(text(sql))
+    topics = result.fetchall()
+    return render_template("index.html", topics=topics)
+
+
+@app.route("/conversation/<int:id>")
+def go_to_conversation(id):
+    sql = "SELECT topic FROM conversations WHERE id=:id"
+    result = db.session.execute(text(sql), {"id":id})
+    topic = result.fetchone()[0]
+    sql = f"SELECT * FROM {topic}"
+    result = db.session.execute(text(sql))
+    messages = result.fetchall()
+    return render_template("conversation.html", topic=topic, messages=messages, id=id)
+
+
+
+@app.route("/conversation/<int:id>/message", methods=["POST","GET"])
+def send_message(id):
+    message = request.form["message"]
+    topic = request.form["topic"]
+    username = session["username"]
+    sql = f"INSERT INTO {topic} (username, message, time) VALUES (:username, :message, NOW())"
+    db.session.execute(text(sql), {"username":username, "message":message})
+    db.session.commit()
+    return redirect(f"/conversation/{id}")
+
 
 @app.route("/login",methods=["POST"])
 def login():
@@ -36,11 +63,13 @@ def login():
             return redirect("/error")
     return redirect("/")
 
+
 @app.route("/logout")
 def logout():
     del session["username"]
     session["admin"]=False
     return redirect("/")
+
 
 @app.route("/register", methods=["POST","GET"])
 def register():
@@ -56,6 +85,7 @@ def register():
     db.session.commit()
     return redirect("/")
 
+
 @app.route("/create", methods=["POST","GET"])
 def create():
     topic = request.form["topic"]
@@ -70,9 +100,21 @@ def create():
     db.session.commit()
     return redirect("/")
 
+
 @app.route("/remove", methods=["POST","GET"])
 def remove():
+    topic = request.form["topic"]
+    check_topic = db.session.execute(text("SELECT Topic FROM conversations WHERE Topic=:topic"), {"topic":topic}).fetchone()
+    if not check_topic:
+        session["error"]="topic_not_found_error"
+        return redirect("/error")
+    sql_conversations = "DELETE FROM conversations WHERE Topic=:topic"
+    db.session.execute(text(sql_conversations), {"topic":topic})
+    sql_remove = "DROP TABLE " + topic
+    db.session.execute(text(sql_remove))
+    db.session.commit()
     return redirect("/")
+
 
 @app.route("/error")
 def error():
