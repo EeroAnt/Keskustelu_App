809a3a36c60782415818ab352aa80d37c853ed46
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-12-03 11:59:22 +0200

    refactor and tidy things

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from os import getenv
 from src.db import db
+
+
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv('DATABASE_URI')
 db.init_app(app)
+
+
 import src.routes
